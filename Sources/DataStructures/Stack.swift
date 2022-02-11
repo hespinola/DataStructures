f1d8e5bdadc783991df10389f143e44bcae917f4
Humberto Espinola <h.espinola@icloud.com>
2022-02-10 18:16:53 -0600

    Add documentation to Stack

--- Sources/DataStructures/Stack.swift
+++ Sources/DataStructures/Stack.swift
@@ -9,22 +9,33 @@ public struct Stack<T> {
         self.items = items
     }
     
+    /// Return the size of the Stack
     public var size: Int {
         items.count
     }
     
+    /// Determine if the Stack is empty
     public var isEmpty: Bool {
         top() == nil
     }
     
+    /// Push a new value to the end of the stack
+    /// Complexity: O(1)
+    /// - Parameter value: Value to be added
     public mutating func push(_ value: T) {
         items.append(value)
     }
     
+    /// Retrieve and remove the last element from the Stack
+    /// Complexity: O(1)
+    /// - Returns: Value if it exists
     public mutating func pop() -> T? {
         items.popLast()
     }
     
+    /// Retrieve the last element from the Stack
+    /// Complexity: O(1)
+    /// - Returns: Value if it exists
     public func top() -> T? {
         items.last
     }
