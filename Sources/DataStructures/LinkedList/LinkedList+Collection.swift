ce239eaa2e4ae447b71e518199ea70f455efb36c
Humberto Espinola <h.espinola@icloud.com>
2022-02-11 00:57:43 -0600

    Conform list to collection protocol

---
+++ Sources/DataStructures/LinkedList/LinkedList+Collection.swift
@@ -0,0 +1,41 @@
+/// Conform to Collection Protocol
+extension LinkedList: Collection {
+    public struct Index: Comparable {
+        public var node: LinkedListNode<T>?
+        
+        static public func ==(lhs: Index, rhs: Index) -> Bool {
+            switch (lhs.node, rhs.node) {
+            case let (left?, right?):
+                return left.next === right.next
+            case (nil, nil):
+                return true
+            default:
+                false
+            }
+        }
+        
+        static public func <(lhs: Index, rhs: Index) -> Bool {
+            guard lhs != rhs else {
+                return false
+            }
+            let nodes = sequence(first: lhs.node) { $0?.next }
+            return nodes.contains { $0 === rhs.node }
+        }
+    }
+    
+    public var startIndex: Index {
+        Index(node: head)
+    }
+    
+    public var endIndex: Index {
+        Index(node: tail?.next)
+    }
+    
+    public func index(after i: Index) -> Index {
+        Index(node: i.node?.next)
+    }
+    
+    public func subscript(position: Index) -> T {
+        position.node!.value
+    }
+}
