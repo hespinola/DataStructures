4b2f848b863e672168c6cb915196f563e06c8b09
Humberto Espinola <h.espinola@icloud.com>
2022-02-11 23:10:25 -0600

    add documentation to queue implementation

--- Sources/DataStructures/Queue.swift
+++ Sources/DataStructures/Queue.swift
@@ -1,22 +1,34 @@
 public struct Queue<T> where T: Equatable {
+    
+    /// Internal queue storage
     private var storage: LinkedList<T>
     
     public init() {
         storage = LinkedList()
     }
     
+    /// isEmpty: Returns true if queue is empty
     public var isEmpty: Bool {
         storage.isEmpty
     }
     
+    /// Appends a value at the end of the queue
+    /// Complexity: O(!)
+    /// - Parameter value: Value to be added to the queue
     public mutating func append(_ value: T) {
         storage.append(value)
     }
     
+    /// Returns and removes the first element in the queue
+    /// Complexity: O(1)
+    /// - Returns: The first element of the queue if any
     public mutating func pop() -> T? {
         storage.pop()
     }
     
+    /// Returns the first element of the queue
+    /// Complexity: O(1)
+    /// - Returns: Returns the first element in the queue if any
     public func peek() -> T? {
         storage.head?.value
     }
