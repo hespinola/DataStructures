bde8845f8bad876044b93dc041cdff17520ca1a6
Humberto Espinola <h.espinola@icloud.com>
2022-02-16 01:00:20 -0600

    Refactor Queue

--- Sources/DataStructures/Queue.swift
+++ Sources/DataStructures/Queue.swift
@@ -1,35 +1,36 @@
-public struct Queue<T> where T: Equatable {
-    
+public struct Queue<T> {
     /// Internal queue storage
-    private var storage: LinkedList<T>
+    private var list = LinkedList<T>()
     
-    public init() {
-        storage = LinkedList()
-    }
+    public init() {}
     
     /// isEmpty: Returns true if queue is empty
     public var isEmpty: Bool {
-        storage.isEmpty
+        list.isEmpty
     }
     
     /// Appends a value at the end of the queue
-    /// Complexity: O(!)
+    /// Complexity: O(1)
     /// - Parameter value: Value to be added to the queue
     public mutating func append(_ value: T) {
-        storage.append(value)
+        list.addLast(value)
     }
     
     /// Returns and removes the first element in the queue
     /// Complexity: O(1)
     /// - Returns: The first element of the queue if any
     public mutating func pop() -> T? {
-        storage.pop()
+        defer {
+            list.removeFirst()
+        }
+        
+        return list.head?.value
     }
     
     /// Returns the first element of the queue
     /// Complexity: O(1)
     /// - Returns: Returns the first element in the queue if any
     public func peek() -> T? {
-        storage.head?.value
+        list.head?.value
     }
 }
